4dcc1c4976e20282972b0a0e7f6220c6d31dca55
Hidayatullah Gustiawan Adi <adihidayatullah96.ah@gmail.com>
2022-05-15 18:52:58 +0700

    menambahkan index pada dashboard, fixed (header, fooder, side, main)

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        return view ('dashboard.index');
+        // return view('user.profile', [
+        //     'user' => User::findOrFail($id)
+        // ]);
+
+    }
+}
---
+++ resources/views/dashboard/main/index.blade.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AdminLTE 3 | Dashboard</title>
+
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+
+    <link rel="stylesheet" href="{{ asset('admin-lte/plugins/fontawesome-free/css/all.min.css') }}">
+
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+
+    <link rel="stylesheet"
+        href="{{ asset('plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('admin-lte') }}/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+
+    <link rel="stylesheet" href="{{ asset('admin-lte') }}/plugins/jqvmap/jqvmap.min.css">
+
+    <link rel="stylesheet" href="{{ asset('admin-lte') }}/dist/css/adminlte.min.css?v=3.2.0">
+
+    <link rel="stylesheet" href="{{ asset('admin-lte') }}/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+
+    <link rel="stylesheet" href="{{ asset('admin-lte') }}/plugins/daterangepicker/daterangepicker.css">
+
+    <link rel="stylesheet" href="{{ asset('admin-lte') }}/plugins/summernote/summernote-bs4.min.css">
+    <script nonce="606600a2-3964-427b-b1d4-e8e80c65ebfb">
+        (function (w, d) {
+            ! function (a, e, t, r) {
+                a.zarazData = a.zarazData || {}, a.zarazData.executed = [], a.zaraz = {
+                    deferred: []
+                }, a.zaraz.q = [], a.zaraz._f = function (e) {
+                    return function () {
+                        var t = Array.prototype.slice.call(arguments);
+                        a.zaraz.q.push({
+                            m: e,
+                            a: t
+                        })
+                    }
+                };
+                for (const e of ["track", "set", "ecommerce", "debug"]) a.zaraz[e] = a.zaraz._f(e);
+                a.addEventListener("DOMContentLoaded", (() => {
+                    var t = e.getElementsByTagName(r)[0],
+                        z = e.createElement(r),
+                        n = e.getElementsByTagName("title")[0];
+                    for (n && (a.zarazData.t = e.getElementsByTagName("title")[0].text), a.zarazData.x =
+                        Math.random(), a.zarazData.w = a.screen.width, a.zarazData.h = a.screen.height,
+                        a.zarazData.j = a.innerHeight, a.zarazData.e = a.innerWidth, a.zarazData.l = a
+                        .location.href, a.zarazData.r = e.referrer, a.zarazData.k = a.screen.colorDepth,
+                        a.zarazData.n = e.characterSet, a.zarazData.o = (new Date).getTimezoneOffset(),
+                        a.zarazData.q = []; a.zaraz.q.length;) {
+                        const e = a.zaraz.q.shift();
+                        a.zarazData.q.push(e)
+                    }
+                    z.defer = !0;
+                    for (const e of [localStorage, sessionStorage]) Object.keys(e).filter((a => a
+                        .startsWith("_zaraz_"))).forEach((t => a.zarazData["z_" + t.slice(7)] = JSON
+                        .parse(e.getItem(t))));
+                    z.referrerPolicy = "origin", z.src = "/cdn-cgi/zaraz/s.js?z=" + btoa(
+                        encodeURIComponent(JSON.stringify(a.zarazData))), t.parentNode.insertBefore(
+                        z, t)
+                }))
+            }(w, d, 0, "script");
+        })(window, document);
+
+    </script>
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+    <div class="wrapper">
+
+        <div class="preloader flex-column justify-content-center align-items-center">
+            <img class="animation__shake" src="{{ asset('admin-lte') }}/dist/img/AdminLTELogo.png" alt="AdminLTELogo"
+                height="60" width="60">
+        </div>
+
+        {{-- HEADER --}}
+        @include('dashboard.main.header')
+        {{-- END HEADER --}}
+        
+        {{-- SIDE --}}
+        @include('dashboard.main.side')
+        {{-- END-SIDE --}}
+
+        <div class="content-wrapper">
+
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1 class="m-0">Dashboard</h1>
+                        </div>
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                <li class="breadcrumb-item active">Dashboard v1</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <section class="content">
+                <div class="container-fluid">
+                    @yield('content')
+                </div>
+            </section>
+
+        </div>
+
+        <footer class="main-footer">
+            <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+            All rights reserved.
+            <div class="float-right d-none d-sm-inline-block">
+                <b>Version</b> 3.2.0
+            </div>
+        </footer>
+
+        <aside class="control-sidebar control-sidebar-dark">
+
+        </aside>
+
+    </div>
+
+
+    {{-- INDEX --}}
+    <script src="{{ asset('admin-lte') }}/plugins/jquery/jquery.min.js"></script>
+
+    <script src="{{ asset('admin-lte') }}/plugins/jquery-ui/jquery-ui.min.js"></script>
+
+    <script>
+        $.widget.bridge('uibutton', $.ui.button)
+
+    </script>
+
+    <script src="{{ asset('admin-lte') }}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <script src="{{ asset('admin-lte') }}/plugins/chart.js/Chart.min.js"></script>
+
+    <script src="{{ asset('admin-lte') }}/plugins/sparklines/sparkline.js"></script>
+
+    <script src="{{ asset('admin-lte') }}/plugins/jqvmap/jquery.vmap.min.js"></script>
+    <script src="{{ asset('admin-lte') }}/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+
+    <script src="{{ asset('admin-lte') }}/plugins/jquery-knob/jquery.knob.min.js"></script>
+
+    <script src="{{ asset('admin-lte') }}/plugins/moment/moment.min.js"></script>
+    <script src="{{ asset('admin-lte') }}/plugins/daterangepicker/daterangepicker.js"></script>
+
+    <script src="{{ asset('admin-lte') }}/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js">
+    </script>
+
+    <script src="{{ asset('admin-lte') }}/plugins/summernote/summernote-bs4.min.js"></script>
+
+    <script src="{{ asset('admin-lte') }}/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+
+    <script src="{{ asset('admin-lte') }}/dist/js/adminlte.js?v=3.2.0"></script>
+
+    <script src="{{ asset('admin-lte') }}/dist/js/demo.js"></script>
+
+    <script src="{{ asset('admin-lte') }}/dist/js/pages/dashboard.js"></script>
+    {{-- END INDEX --}}
+
+</body>
+
+</html>
