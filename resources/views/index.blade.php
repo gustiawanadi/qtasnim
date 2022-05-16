990519c44b070f10eaf36e6a50dddffb4be372a1
Hidayatullah Gustiawan Adi <adihidayatullah96.ah@gmail.com>
2022-05-17 00:32:24 +0700

    menambahkan fitur login dan frondEnd

---
+++ resources/views/index.blade.php
@@ -0,0 +1,47 @@
+@extends('main.index')
+@section('content')
+<div class="row">
+    <div class="col-lg-4 col-12">
+
+        <div class="small-box bg-info">
+            <div class="inner">
+                <h3>150</h3>
+                <p>Barang</p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-bag"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+    </div>
+
+    <div class="col-lg-4 col-12">
+
+        <div class="small-box bg-success">
+            <div class="inner">
+                <h3>53<sup style="font-size: 20px">%</sup></h3>
+                <p>Kategori</p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-stats-bars"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+    </div>
+
+    <div class="col-lg-4 col-12">
+
+        <div class="small-box bg-danger">
+            <div class="inner">
+                <h3>65</h3>
+                <p>Transaksi</p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-pie-graph"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+    </div>
+
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,8 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\TransactionController;
-use Illuminate\Support\Facades\Route;
+// use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,13 +18,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/dashboard', ([DashboardController::class, 'index']));
-Route::resource('/dashboard/items', ItemController::class);
-Route::resource('/dashboard/categories', CategoryController::class);
-Route::resource('/dashboard/transactions', TransactionController::class);
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Auth::routes();
-
+Route::get('/', [App\Http\Controllers\FrondEndController::class, 'index']);
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/dashboard', ([DashboardController::class, 'index']))->middleware('auth');;
+Route::resource('/dashboard/items', ItemController::class)->middleware('auth');
+Route::resource('/dashboard/categories', CategoryController::class)->middleware('auth');
+Route::resource('/dashboard/transactions', TransactionController::class)->middleware('auth');
+
