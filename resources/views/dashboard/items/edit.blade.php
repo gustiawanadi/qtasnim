f674fdf455bea81c2c2ffcaf430134bdc1be3f55
Hidayatullah Gustiawan Adi <adihidayatullah96.ah@gmail.com>
2022-05-16 11:01:17 +0700

    mengganti card-title dari "tambah" ke "edit Data"

--- resources/views/dashboard/items/edit.blade.php
+++ resources/views/dashboard/items/edit.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class="card card-default">
     <div class="card-header">
-        <h3 class="card-title">Tambah Barang</h3>
+        <h3 class="card-title">Edit Data Barang</h3>
     </div>
 
     <div class="card-body">
