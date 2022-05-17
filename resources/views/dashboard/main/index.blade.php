c374cb670d754f5b843c561cd516252050952a44
Hidayatullah Gustiawan Adi <adihidayatullah96.ah@gmail.com>
2022-05-17 23:35:08 +0700

    menambah function searching di index-dashboard

--- resources/views/dashboard/main/index.blade.php
+++ resources/views/dashboard/main/index.blade.php
@@ -174,6 +174,7 @@
                 "responsive": true,
                 "lengthChange": false,
                 "autoWidth": false,
+                "searching": true,
                 // "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
             }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
             $('#example2').DataTable({
